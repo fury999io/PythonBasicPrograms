a3ffb96286e0fcd0f09530e59bbc9dd524d54e16
FURY 999 <76838573+fury999io@users.noreply.github.com>
2022-08-24 10:09:17 +0530

    Add files via upload

---
+++ FibonacciSeries.py
@@ -0,0 +1,15 @@
+#To generate the Fibonacci Series upto the N th term
+
+print("Enter the value of N")
+N=int(input())
+
+a=0
+b=1
+
+count=1
+
+while(count<=N):
+    c=a+b
+    print(c, end=", ")
+    a, b = b, c
+    count=count+1
